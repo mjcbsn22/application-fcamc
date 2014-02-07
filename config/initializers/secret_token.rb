945b4f9b3ed3e7fe4c9ff78582c4ce0b9447c7af
unknown <mattjacobson@collectivebias.com>
2014-02-06 20:46:00 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApplicationFcamc::Application.config.secret_key_base = '0e9a8385ac1516de5e2a9835a496a08e9e47c856e556e38f0afacbbb1aa3947bb6c1eb26b7709067de0f958e12a7f72911f75337090730eae29e6e57e711f6b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApplicationFcamc::Application.config.session_store :cookie_store, key: '_application-fcamc_session'
